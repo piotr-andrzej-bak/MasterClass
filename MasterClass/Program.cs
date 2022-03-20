10daf7ebdb87c3b3b13ab0d7567d31d84b23efe1
Piotr Bąk <piotr.andrzej.bak@gmail.com>
2022-03-20 18:24:58 +0100

    v0.02

--- MasterClass/Program.cs
+++ MasterClass/Program.cs
@@ -6,7 +6,33 @@ namespace MasterClass
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int num1 = 13;
+            int num2 = 23;
+            int sum = num1 + num2;
+            Console.WriteLine($"num1 {num1}" + ", num2 " + num2);
+            //int num3, num4, num5;
+
+            ///<summary>
+            ///
+            /// 
+            /// 
+            /// </summary>
+
+            //string test = "test";
+            //test = Console.ReadLine();
+            //Console.WriteLine(test);
+            Console.WriteLine("---------------------------");
+
+            Console.WriteLine("Enter a string and press enter: ");
+            string readInput = Console.ReadLine();
+            Console.WriteLine("You have entered: " + readInput);
+            Console.WriteLine("You have entered: {0}", readInput);
+
+            Console.Write("Enter a string and press enter: ");
+            int asciiValue = Console.Read();
+            Console.WriteLine("ASCII value is: " + asciiValue);
+
+
         }
     }
 }
